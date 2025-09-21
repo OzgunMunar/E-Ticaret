57847667e5de65ff2a5e4916d7450d035ef05587
Ozgun Munar <ozgunmnr@gmail.com>
2025-09-21 14:21:00 +0300

    Urunleri sinirlandirarak listelendi ve scroll ile beslendi

--- apps/ui/src/pages/home/home.ts
+++ apps/ui/src/pages/home/home.ts
@@ -1,11 +1,13 @@
 import { httpResource } from '@angular/common/http';
-import { ChangeDetectionStrategy, Component, computed, effect, ViewEncapsulation } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, effect, signal, ViewEncapsulation } from '@angular/core';
 import { ProductModel } from "@/shared/product.model"
 import { TrCurrencyPipe } from 'tr-currency'
+import { InfiniteScrollDirective } from 'ngx-infinite-scroll'
 
 @Component({
   imports: [
-    TrCurrencyPipe
+    TrCurrencyPipe,
+    InfiniteScrollDirective
   ],
   templateUrl: './home.html',
   encapsulation: ViewEncapsulation.None,
@@ -14,8 +16,30 @@ import { TrCurrencyPipe } from 'tr-currency'
 
 export default class Home {
 
-  readonly result = httpResource<ProductModel[]>(() => "apiUrl/products")
-  readonly data = computed(() => this.result.value() ?? [])
+  readonly limit = signal<number>(6)
+  readonly start = signal<number>(0)
+  readonly result = httpResource<ProductModel[]>(
+  () => {
+      const endpoint = `apiUrl/products?_limit=${this.limit()}&_start=${this.start()}`
+      return endpoint
+  });
 
+  readonly data = computed(() => this.result.value() ?? [])
+  readonly dataSignal = signal<ProductModel[]>([])
+
+  constructor() {
+
+    effect(() => {
+
+      this.dataSignal.update(prevVal => [...prevVal, ...this.data()])
+
+    })
+
+  }
+
+  onScroll() {
+    this.limit.update(prevVal => prevVal + 6)
+    this.start.update(prevVal => prevVal + 6)
+  }
 
 }
