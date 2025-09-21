dafbed513b3954af1e5384b15fbd6fdf0654f1d2
Ozgun Munar <ozgunmnr@gmail.com>
2025-09-21 12:21:14 +0300

    login islemi tamamlandi.

--- apps/admin/src/pages/login/login.ts
+++ apps/admin/src/pages/login/login.ts
@@ -1,11 +1,45 @@
-import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { ChangeDetectionStrategy, Component, inject, ViewEncapsulation } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { UserModel } from '../users/users';
+import { FlexiToastService } from 'flexi-toast';
+import { Router } from '@angular/router';
 
 @Component({
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './login.html',
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush
 })
+
 export default class Login {
+
+  readonly #http = inject(HttpClient)
+  readonly #toast = inject(FlexiToastService)
+  readonly #router = inject(Router)
+
+  signIn(form: NgForm) {
+
+    if(!form.valid) return
+
+    const params = new HttpParams()
+      .set('userName', form.value['userName'])
+      .set('password', form.value['password']);
+
+    this.#http.get<UserModel[]>('apiUrl/users', { params }).subscribe((res) => {
+
+      if(res.length === 0) {
+        this.#toast.showToast("Hata","Kullanıcı adı veya şifre yanlış!","error")
+        return
+      }
+
+      localStorage.setItem("response", JSON.stringify(res[0]))
+      this.#router.navigateByUrl("/")
+
+    })
+
+  }
 
 }
