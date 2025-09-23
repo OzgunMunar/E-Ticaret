84c6774a63bec72d09a77285e5ed38296bf5d746
Ozgun Munar <ozgunmnr@gmail.com>
2025-09-23 16:18:42 +0300

    Sepet sayfasi tamamlandi.

--- apps/ui/src/pages/basket/basket.ts
+++ apps/ui/src/pages/basket/basket.ts
@@ -1,8 +1,9 @@
-import { httpResource } from '@angular/common/http';
+import { HttpClient, httpResource } from '@angular/common/http';
 import { ChangeDetectionStrategy, Component, computed, effect, inject, ViewEncapsulation } from '@angular/core';
 import { Common } from '../../services/common';
 import { BasketModel } from '@/shared/basket.model';
 import { TrCurrencyPipe } from 'tr-currency';
+import { FlexiToastService } from 'flexi-toast';
 
 @Component({
   imports: [
@@ -16,7 +17,7 @@ export default class Basket {
 
   readonly #common = inject(Common)
   readonly total = computed(() => {
-    
+
     let val = 0
 
     this.data().forEach((res) => {
@@ -48,5 +49,47 @@ export default class Basket {
   })
 
   readonly data = computed(() => this.result.value() ?? [])
+  readonly #http = inject(HttpClient)
+  readonly #toast = inject(FlexiToastService)
+
+  increment(val: BasketModel) {
+
+    const updated = { ...val, quantity: val.quantity + 1 }
+    this.#http.put(`apiUrl/baskets/${val.id}`, updated).subscribe(() => {
+
+      this.result.reload()
+
+    })
+
+  }
+
+  decrease(val: BasketModel) {
+
+    const updated = { ...val, quantity: val.quantity - 1 }
+    if (val.quantity - 1 <= 0) {
+
+      this.#toast.showSwal("Sil?", "Ürünü sepetten sikmek istediğinize emin misiniz?", "Sil", () => {
+
+        this.#http.delete(`apiUrl/baskets/${val.id}`).subscribe(() => {
+
+          this.result.reload()
+
+        })
+
+      })
+
+    } else {
+
+      const updated = { ...val, quantity: val.quantity - 1 }
+
+      this.#http.put(`apiUrl/baskets/${val.id}`, updated).subscribe(() => {
+
+        this.result.reload()
+
+      })
+
+    }
+
+  }
 
 }
